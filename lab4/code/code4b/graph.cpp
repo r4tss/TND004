f983ebd12aedd3d27ed1f70c28219787b3e00c10
Edvin Jakobsson <edvj7467@gmail.com>
2024-05-13 14:44:50 +0200

    Completed Prim's algorithm

---
+++ lab4/code/code4b/graph.cpp
@@ -0,0 +1,138 @@
+/*********************************************
+ * file:	~\code4b\graph.h                  *
+ * remark:implementation of undirected graphs *
+ **********************************************/
+
+#include <iostream>
+#include <algorithm>
+#include <format>
+#include <cassert>     // assert
+#include <limits>      // std::numeric_limits
+#include <algorithm>   // std::make_heap(), std::pop_heap(), std::push_heap()
+
+#include "graph.h"
+#include "dsets.h"
+
+// Note: graph vertices are numbered from 1 -- i.e. there is no vertex zero
+
+// -- CONSTRUCTORS
+
+// Create a graph with n vertices and no vertices
+Graph::Graph(int n) : table(n + 1), size{n}, n_edges{0} {
+    assert(n >= 1);
+}
+
+Graph::Graph(const std::vector<Edge> &V, int n) : Graph{n} {
+    for (auto e : V) {
+        insertEdge(e);
+    }
+}
+
+// -- MEMBER FUNCTIONS
+
+// insert undirected edge e
+// update weight if edge e is present
+void Graph::insertEdge(const Edge &e) {
+    assert(e.from >= 1 && e.from <= size);
+    assert(e.to >= 1 && e.to <= size);
+
+    auto edge_insertion = [&T = this->table, &n = this->n_edges](const Edge &e1) {
+        if (auto it = std::find_if(begin(T[e1.from]), end(T[e1.from]),
+                                   [e1](const Edge &ed) { return e1.links_same_nodes(ed); });
+            it == end(T[e1.from])) {
+            T[e1.from].push_back(e1);  // insert new edge e1
+            ++n;                       // increment the counter of edges
+        } else {
+            it->weight = e1.weight;  // update the weight
+        }
+    };
+
+    edge_insertion(e);
+    edge_insertion(e.reverse());
+}
+
+// remove undirected edge e
+void Graph::removeEdge(const Edge &e) {
+    assert(e.from >= 1 && e.from <= size);
+    assert(e.to >= 1 && e.to <= size);
+
+    auto edgeRemoval = [&T = this->table, &n = this->n_edges](const Edge &e1) {
+        auto it = std::find_if(begin(T[e1.from]), end(T[e1.from]),
+                               [e1](const Edge &ed) { return e1.links_same_nodes(ed); });
+
+        assert(it != end(T[e1.from]));
+        T[e1.from].erase(it);  // remove edge e1
+        --n;                   // decrement the counter of edges
+    };
+
+    edgeRemoval(e);
+    edgeRemoval(e.reverse());
+}
+
+// Prim's minimum spanning tree algorithm
+void Graph::mstPrim() const {
+    std::vector<int> dist(size + 1, std::numeric_limits<int>::max());
+    std::vector<int> path(size + 1, 0);
+    std::vector<bool> done(size + 1, false);
+
+    dist[1] = 0;
+    done[1] = true;
+    int v = 1;
+
+    while(true)
+    {
+        for(auto& e : table[v])
+        {
+            int u = e.to;
+
+            if(done[u] == false && dist[u] > e.weight)
+            {
+                path[u] = v;
+                dist[u] = e.weight;
+            }
+        }
+
+        v = 0;
+        
+        for(int i = 0;i <= size;i++)
+        {
+            if(done[i] == false && (v == 0 || dist[v] > dist[i]))
+            {
+                v = i;
+            }
+        }
+        
+        if(v == 0)
+            break;
+
+        done[v] = true;
+    }
+}
+
+// Kruskal's minimum spanning tree algorithm
+void Graph::mstKruskal() const {
+//    std::make_heap()
+    
+    int counter = 0;
+
+    //while(counter < size - 1) // Might have to be just size
+    //{
+        
+    //}
+}
+
+// print graph
+void Graph::printGraph() const {
+    std::cout << std::format("{:-<66}\n", '-');
+    std::cout << "Vertex  adjacency lists\n";
+    std::cout << std::format("{:-<66}\n", '-');
+
+    for (int v = 1; v <= size; v++) {
+        std::cout << std::format("{:4} : ", v);
+        for (auto const &e : table[v]) {
+            std::cout << std::format("({:2}, {:2}) ", e.to, e.weight);
+        }
+        std::cout << "\n";
+    }
+    std::cout << std::format("{:-<66}\n", '-');
+}
